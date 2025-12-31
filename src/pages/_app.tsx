68baf1655e9fc7044da5613c2b9a324d7d67fb77
mantra-info <albie@mantraitsolutions.in>
2025-12-31 14:03:01 +0530

    feat(updates): file type to webp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,13 @@ import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import PreLoader from "./components/PreLoader"; // Import normally
 import ScrollToTop from "./components/ScrollToTop";
+import { Instrument_Sans } from 'next/font/google'
 
-
-
+const instrumentSans = Instrument_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-instrument',
+})
 export default function App({ Component, pageProps }: AppProps) {
  
   return (
@@ -14,11 +18,13 @@ export default function App({ Component, pageProps }: AppProps) {
         Show loader if loading is true. 
         Wrap content in a div that controls visibility 
       */}
-      
-        <div className="animate-in fade-in duration-700">
+         <main className={instrumentSans.variable}>
+     <div className="animate-in fade-in duration-700">
           <Component {...pageProps} />
           <ScrollToTop />
         </div>
+         </main>
+   
      
     </>
   );
